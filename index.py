da47c89c26509de5b690575d9862bd2220041266
WeslleyIvis <weslley_reive@hotmail.com>
2023-11-13 19:22:45 -0300

    Interface

---
+++ index.py
@@ -0,0 +1,17 @@
+import sys
+from PyQt5.QtWidgets import QApplication, QMainWindow
+from cad import Ui_MainWindow
+
+class App(QMainWindow):
+    def __init__(self):
+        super().__init__()
+
+        self.ui = Ui_MainWindow()
+        self.ui.setupUi(self)
+
+
+if __name__ == "__main__":
+    app = QApplication(sys.argv)
+    window = App()
+    window.show()
+    sys.exit(app.exec_())    
